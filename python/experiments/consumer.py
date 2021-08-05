62ffb5c8001b81b1bfc43c3fdacbf8f2949d5a7f
Piotr Krzeminski <git@krzeminski.it>
2021-08-05 21:43:14 +0200

    Call newCode() in consumer.py

--- python/experiments/consumer.py
+++ python/experiments/consumer.py
@@ -1,7 +1,9 @@
 from out_ir import returnString
 from out_ir import returnStringFromClass
 from out_ir import returnParameterFromClass
+from out_ir import newCode
 
 print(returnString())
 print(returnStringFromClass())
 print(returnParameterFromClass())
+newCode()
