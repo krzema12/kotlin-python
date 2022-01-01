7aa6d5f86e5d8901911fd4e062c55624ba70eae6
Piotr Krzeminski <git@krzeminski.it>
2022-01-01 21:21:12 +0100

    [#103] Remove unneeded dependencies of CLI module
    There's still `:compiler:backend.wasm`, but I didn't manage to get rid
    of it easily because the JS backend contains some needed stuff.

--- compiler/cli/cli-py/build.gradle.kts
+++ compiler/cli/cli-py/build.gradle.kts
@@ -4,19 +4,10 @@ plugins {
 }
 
 dependencies {
-    compile(project(":compiler:util"))
-    compile(project(":compiler:cli-common"))
     compile(project(":compiler:cli"))
-    compile(project(":compiler:frontend"))
-    compile(project(":compiler:backend-common"))
-    compile(project(":compiler:ir.backend.common"))
-    compile(project(":compiler:ir.serialization.js"))
     compile(project(":compiler:ir.tree.impl"))
     compile(project(":compiler:backend.py"))
     compile(project(":compiler:backend.wasm"))
-    compile(project(":python:py.translator"))
-    compile(project(":js:js.serializer"))
-    compile(project(":js:js.dce"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
 }
