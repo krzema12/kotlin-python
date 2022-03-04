6226e842f32c07f6547c447498f766240f6ff331
Piotr Krzeminski <git@krzeminski.it>
2022-03-04 08:44:53 +0100

    [#74] Add end-to-end tests for main function

--- python/e2e-tests/run.sh
+++ python/e2e-tests/run.sh
@@ -5,6 +5,32 @@ mkdir -p python/e2e-tests/out
 
 echo "Starting end-to-end tests"
 
+# With Kotlin file with 'main' function
+for testcaseFile in python/e2e-tests/testcases/*.kt.txt; do
+    baseFileName=$(basename -- "$testcaseFile")
+    testName="${baseFileName%%.*}"
+    echo "===== Executing '$testName' ====="
+
+    awk '/--- Kotlin code ---/,/--- Expected output ---/' "$testcaseFile" | tail -n +2 | head -n -1 > python/e2e-tests/out/kotlin-code.kt
+    awk '/--- Expected output ---/,0' "$testcaseFile" | tail -n +2 > python/e2e-tests/out/expected-output.txt
+
+    echo "  Compiling to Python..."
+    dist/kotlinc/bin/kotlinc-py \
+      -libraries dist/kotlinc/lib/kotlin-stdlib-js.jar \
+      -Xir-produce-py \
+      -output python/e2e-tests/out/compiled.py \
+      python/e2e-tests/out/kotlin-code.kt
+
+    echo "  Running Python to produce the output..."
+    python3 python/e2e-tests/out/compiled.py some arguments 123 > python/e2e-tests/out/output.txt
+
+    echo "  Comparing actual and expected output..."
+    diff -u python/e2e-tests/out/expected-output.txt python/e2e-tests/out/output.txt
+
+    echo "===== Finished with '$testName' ====="
+done
+
+# With Python consumer
 for testcaseFile in python/e2e-tests/testcases/*.kt.py.txt; do
     baseFileName=$(basename -- "$testcaseFile")
     testName="${baseFileName%%.*}"
